831cc04d438368e6d1d0ac5c71cbb0c35240cfee
lcsgborges <lcsgborges@gmail.com>
2025-11-11 12:43:55 -0300

    Introdução linguagem c e c++

--- arrays-matrizes-structs/matrizes.c
+++
@@ -1,3 +0,0 @@
-// Matrizes são vetores bidimensionais:
-
-// Matriz[][] = isso é uma matriz em C. Não precisamos declarar a quantidade de linhas, mas somos obrigados a passar a quantidade de colunas;
